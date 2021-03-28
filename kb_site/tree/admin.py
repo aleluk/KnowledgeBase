ac824fda845816a94f90ae6053370c679dc2a8d4
Aleksandra <64218199+aleluk@users.noreply.github.com>
2021-03-28 09:59:56 +0200

    First commit

---
+++ kb_site/tree/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import (
+    UpdateSecurity,
+    WindowsUpdate,
+    FeatureUpdate,
+    WindowsUpdateError,
+    FeatureUpdateError,
+    AboutWindowsUpdate,
+    AboutFeatureUpdate,
+)
+
+# Register your models here.
+admin.site.register(UpdateSecurity)
+admin.site.register(WindowsUpdate)
+admin.site.register(FeatureUpdate)
+admin.site.register(WindowsUpdateError)
+admin.site.register(FeatureUpdateError)
+admin.site.register(AboutWindowsUpdate)
+admin.site.register(AboutFeatureUpdate)
